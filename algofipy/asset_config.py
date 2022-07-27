83d291b008e0668274c8da54f26377078a4639f2
Armstrong Park <goarmstrongpark.ap@gmail.com>
2022-07-27 14:07:48 -0400

    also added asset configs

--- algofipy/asset_config.py
+++ algofipy/asset_config.py
@@ -13,7 +13,16 @@ class AssetConfig:
 
 ASSET_CONFIGS = {
     Network.MAINNET : {
-        1 : AssetConfig("ALGO", 1, 6)
+        1: AssetConfig("ALGO", 1, 6),
+        818179690: AssetConfig("AF-BANK-ALGO-STANDARD", 818179690, 6),
+        31566704: AssetConfig("USDC", 818179690, 6),
+        818182311: AssetConfig("AF-BANK-USDC-STANDARD", 818182311, 6),
+        386192725 : AssetConfig("GOBTC", 386192725, 8),
+        818184214: AssetConfig("AF-BANK-GOBTC-STANDARD", 818184214, 6),
+        386195940: AssetConfig("GOETH", 386195940, 8),
+        818188553: AssetConfig("AF-BANK-GOETH-STANDARD", 818188553, 6),
+        312769: AssetConfig("USDT", 312769, 6),
+        818190568: AssetConfig("AF-BANK-USDT-STANDARD", 818190568, 6)
     },
     Network.MAINNET_CLONE : {
         1 : AssetConfig("ALGO", 1, 6),
