749e4e16cacb24b45624d19e95403e576812df69
John Clarke <john@algofi.org>
2022-09-14 22:29:44 +0000

    added pool interface fcn

--- algofipy/interfaces/lending_pool_interface.py
+++ algofipy/interfaces/lending_pool_interface.py
@@ -2,7 +2,7 @@
 
 # external
 from algosdk import logic
-from algosdk.future.transaction import ApplicationNoOpTxn
+from algosdk.future.transaction import ApplicationNoOpTxn, LogicSigAccount
 
 # local
 from .lending_pool_interface_config import LENDING_POOL_INTERFACE_STRINGS
@@ -13,13 +13,16 @@ from algofipy.amm.v1.pool import Pool
 from algofipy.amm.v1.balance_delta import BalanceDelta
 from algofipy.amm.v1.asset import Asset
 from algofipy.amm.v1.amm_config import PoolType
-from algofipy.transaction_utils import get_payment_txn
+from algofipy.transaction_utils import get_payment_txn, get_default_params, TransactionGroup
+from algofipy.utils import int_to_bytes
 
 # INTERFACE
 
 # constants
 
-# TODO: THIS IS A WORK IN PROGRESS
+PERMISSIONLESS_SENDER_LOGIC_SIG = LogicSigAccount(
+    bytes([6, 49, 16, 129, 6, 18, 68, 49, 25, 129, 0, 18, 68, 49, 9, 50, 3, 18, 68, 49, 32, 50, 3, 18, 68, 129, 1, 67])
+)
 
 class LendingPoolInterface:
 
@@ -53,7 +56,7 @@ class LendingPoolInterface:
         self.lp_market.load_state()
         self.pool.refresh_state()
 
-    def get_pool_quote(asset_id, amount):
+    def get_pool_quote(self, asset_id, amount):
 
         asset1_pooled_amount = 0
         b_asset1_pooled_amount = 0
@@ -67,7 +70,7 @@ class LendingPoolInterface:
             b_asset1_pooled_amount = self.market1.underlying_to_b_asset(amount)
             pool_quote = self.pool.get_pool_quote(self.market1.b_asset_id, b_asset1_pooled_amount)
             b_asset2_pooled_amount = -1 * pool_quote.asset2_delta
-            asset2_pooled_amount = self.market2.b_asset_to_asset_amount(b_asset2_pooled_amount)
+            asset2_pooled_amount = self.market2.b_asset_to_asset_amount(b_asset2_pooled_amount).underlying
             lps_issued = pool_quote.lp_delta
             num_iter = pool_quote.num_iter
         else:
@@ -75,30 +78,29 @@ class LendingPoolInterface:
             b_asset2_pooled_amount = self.market2.underlying_to_b_asset(amount)
             pool_quote = self.pool.get_pool_quote(self.market2.b_asset_id, b_asset2_pooled_amount)
             b_asset1_pooled_amount = -1 * pool_quote.asset1_delta
-            asset1_pooled_amount = self.market1.b_asset_to_asset_amount(b_asset1_pooled_amount)
+            asset1_pooled_amount = self.market1.b_asset_to_asset_amount(b_asset1_pooled_amount).underlying
             lps_issued = pool_quote.lp_delta
             num_iter = pool_quote.num_iter
 
         return BalanceDelta(self.pool, -1 * asset1_pooled_amount, -1 * asset2_pooled_amount, lps_issued, num_iter)        
 
-    def get_pool_txns(self, sender, quote, asset2_amount, maximum_slippage, add_to_user_collateral=False, params=None):
+    def get_pool_txns(self, user, quote, maximum_slippage, add_to_user_collateral=False, params=None):
 
-        '''
         if params is None:
             params = get_default_params(self.algod)
 
         additional_permisionless_fee = 27000 + quote.num_iter * 1000 + (3000 if add_to_user_collateral else 1000)
 
         # send asset 1
-        txn0 = get_payment_txn(sender, params, self.address, asset1_amount, self.market1.underlying_asset_id)
+        txn0 = get_payment_txn(user.address, params, self.address, -1* quote.asset1_delta, self.market1.underlying_asset_id)
 
         # send asset 2
-        txn1 = get_payment_txn(sender, params, self.address, asset2_amount, self.market2.underlying_asset_id)
+        txn1 = get_payment_txn(user.address, params, self.address, -1 * quote.asset2_delta, self.market2.underlying_asset_id)
 
         # pool step 1
         params.fee = 1000 + additional_permisionless_fee
         txn2 = ApplicationNoOpTxn(
-            sender=sender,
+            sender=user.address,
             sp=params,
             index=self.app_id,
             app_args=[
@@ -111,6 +113,88 @@ class LendingPoolInterface:
         # pool step 2
         params.fee = 0
         txn3 = ApplicationNoOpTxn(
-            
+            sender=PERMISSIONLESS_SENDER_LOGIC_SIG.address(),
+            sp=params,
+            index=self.app_id,
+            app_args=[
+                bytes(LENDING_POOL_INTERFACE_STRINGS.pool_step_2, "utf-8"),
+            ],
+            accounts=[self.market1.address],
+            foreign_apps=[self.market1.app_id, self.market1.manager_app_id],
+            foreign_assets=[self.market1.underlying_asset_id, self.market1.b_asset_id]
         )
-        '''
+
+        # pool step 3
+        params.fee = 0
+        txn4 = ApplicationNoOpTxn(
+            sender=PERMISSIONLESS_SENDER_LOGIC_SIG.address(),
+            sp=params,
+            index=self.app_id,
+            app_args=[
+                bytes(LENDING_POOL_INTERFACE_STRINGS.pool_step_3, "utf-8"),
+            ],
+            accounts=[self.market2.address],
+            foreign_apps=[self.market2.app_id, self.market2.manager_app_id],
+            foreign_assets=[self.market2.underlying_asset_id, self.market2.b_asset_id]
+        )
+
+        # pool step 4
+        params.fee = 0
+        txn5 = ApplicationNoOpTxn(
+            sender=PERMISSIONLESS_SENDER_LOGIC_SIG.address(),
+            sp=params,
+            index=self.app_id,
+            app_args=[
+                bytes(LENDING_POOL_INTERFACE_STRINGS.pool_step_4, "utf-8"),
+                int_to_bytes(maximum_slippage)
+            ],
+            accounts=[self.pool.address],
+            foreign_apps=[self.pool.application_id, self.pool.manager_application_id],
+            foreign_assets=[self.pool.asset1.asset_id, self.pool.asset2.asset_id, self.pool.lp_asset_id]
+        )
+
+        # pool step 5
+        params.fee = 0
+        txn6 = ApplicationNoOpTxn(
+            sender=PERMISSIONLESS_SENDER_LOGIC_SIG.address(),
+            sp=params,
+            index=self.app_id,
+            app_args=[
+                bytes(LENDING_POOL_INTERFACE_STRINGS.pool_step_5, "utf-8"),
+                int_to_bytes(1) if add_to_user_collateral else int_to_bytes(0)
+            ],
+            accounts=[user.address, user.lending.storage_address, self.lp_market.address] if user.lending.opted_in_to_manager else [user.address, self.lp_market.address],
+            foreign_apps=[self.lp_market.app_id, self.lp_market.manager_app_id],
+            foreign_assets=[self.lp_market.underlying_asset_id]
+        )
+
+        # pool step 6
+        params.fee = 0
+        txn7 = ApplicationNoOpTxn(
+            sender=PERMISSIONLESS_SENDER_LOGIC_SIG.address(),
+            sp=params,
+            index=self.app_id,
+            app_args=[
+                bytes(LENDING_POOL_INTERFACE_STRINGS.pool_step_6, "utf-8")
+            ],
+            accounts=[user.address, self.market1.address],
+            foreign_apps=[self.market1.app_id, self.market1.manager_app_id],
+            foreign_assets=[self.market1.underlying_asset_id, self.market1.b_asset_id]
+        )
+
+        # pool step 7
+        params.fee = 0
+        txn8 = ApplicationNoOpTxn(
+            sender=PERMISSIONLESS_SENDER_LOGIC_SIG.address(),
+            sp=params,
+            index=self.app_id,
+            app_args=[
+                bytes(LENDING_POOL_INTERFACE_STRINGS.pool_step_7, "utf-8")
+            ],
+            accounts=[user.address, self.market2.address],
+            foreign_apps=[self.market2.app_id, self.market2.manager_app_id],
+            foreign_assets=[self.market2.underlying_asset_id, self.market2.b_asset_id]
+        )
+
+        return TransactionGroup([txn0, txn1, txn2, txn3, txn4, txn5, txn6, txn7, txn8])
+        
