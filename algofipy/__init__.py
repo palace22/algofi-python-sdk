64e4de942278d925b2bdceafc565bff471021c58
John Clarke <john@algofi.org>
2022-09-15 04:06:04 +0000

    increment version number

--- algofipy/__init__.py
+++ algofipy/__init__.py
@@ -3,5 +3,5 @@ This module contains all the relevant classes and data for interacting with the 
 """
 
 __all__ = ["lending", "amm", "staking", "interfaces", "algofi_client", "algofi_user", "asset_amount", "asset_config", "globals", "state_utils", "transaction_utils", "utils"]
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 __author__ = "Algofi"
