e841bdb317aa6f64cce0f1941e7191ece94c718d
John Clarke <john@algofi.org>
2022-09-22 04:23:58 +0000

    fix mgr app id

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description="Official Algofi Protocol Python SDK",
     author="Algofi",
     author_email="founders@algofi.org",
-    version="2.1.3",
+    version="2.1.4",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
