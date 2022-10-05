ec15a16dd91888ea079016390629a4a90f7bcce8
John Clarke <john@algofi.org>
2022-10-05 22:26:23 +0000

    upload

--- algofipy/governance/v1/admin.py
+++ algofipy/governance/v1/admin.py
@@ -1,7 +1,7 @@
 
 # IMPORTS
 from algosdk import logic
-from algosdk.future.transaction import ApplicationNoOpTxn, AssetTransferTxn
+from algosdk.future.transaction import PaymentTxn, ApplicationNoOpTxn, AssetTransferTxn, ApplicationCloseOutTxn
 
 # INTERFACE
 from algofipy.governance.v1.governance_config import VOTING_ESCROW_STRINGS
@@ -208,5 +208,122 @@ class Admin:
                 voting_user.governance.v1.user_admin_state.storage_address,
                 voting_user.governance.v1.user_admin_state.delegating_to,
                 logic.get_application_address(proposal.app_id)
-            ],
+            ]
         )
+
+        return txn0 + TransactionGroup([txn1])
+
+    def close_out_from_proposal_txns(self, user_calling, user_closing_out, proposal):
+        """Constructs a series of transactions which will close out a target user from a proposal.
+
+        :param user_calling: user who is calling the transaction
+        :type user_calling: :class:`AlgofiUser`
+        :param user_closing_out: user who is closing out
+        :type user_closing_out: :class:`AlgofiUser`
+        :param proposal: proposal being closed out of 
+        :type proposal: :class:`Proposal`
+        :return: a series of transactions which will close out a target user from a proposal.
+        :rtype: :class:`TransactionGroup`
+        """
+
+        params = get_default_params(self.algod)
+
+        params.fee = 3000
+        txn0 = ApplicationCloseOutTxn(
+            sender=user_calling.address,
+            sp=params,
+            index=self.admin_app_id,
+            app_args=[bytes(ADMIN_STRINGS.close_out_from_proposal, "utf-8")],
+            accounts=[logic.get_application_address(proposal.app_id), user_close_out.governance.v1.user_admin_state.storage_address],
+            foreign_apps=[proposal.app_id]
+        )
+
+        return TransactionGroup([txn0])
+
+    def get_set_open_to_delegation_txns(self, user):
+        """Constructs a series of transactions to set a user open to delegation.
+
+        :param user: user who is setting themselves open to delegation
+        :type user: :class:`AlgofiUser`
+        :return: a series of transactions to set a user open to delegation.
+        :rtype: :class:`TransactionGroup`
+        """
+
+        params = get_default_params(self.algod)
+
+        txn0 = ApplicationNoOpTxn(
+            sender=user.address,
+            sp=params,
+            index=self.admin_app_id,
+            app_args=[bytes(ADMIN_STRINGS.set_open_to_delegation, "utf-8")],
+            accounts=[user.governance.v1.user_admin_state.storage_address]   
+        )
+
+        return TransactionGroup([txn0])
+
+    def get_set_not_open_to_delegation_txns(self, user):
+        """Constructs a series of tranactions to set a user not open to delegation.
+
+        :param user: user who is setting themselves not open to delegation
+        :type user: :class:`AlgofiUser`
+        :return: a series of tranactions to set a user not open to delegation.
+        :rtype: :class:`TransactionGroup`
+        """
+
+        params = get_default_params(self.algod)
+
+        txn0 = ApplicationNoOpTxn(
+            sender=user.address,
+            sp=params,
+            index=self.admin_app_id,
+            app_args=[bytes(ADMIN_STRINGS.set_not_open_to_delegation, "utf-8")],
+            accounts=[user.governance.v1.user_admin_state.storage_address]   
+        )
+
+        return TransactionGroup([txn0])
+    
+    def get_create_proposal_txns(self, user, title, link):
+        """Constructs a series of transactions to create a proposal.
+
+        :param user: user who is trying to create the transaction
+        :type user: :class:`AlgofiUser`
+        :param title: title of the proposal to be created
+        :type title: str
+        :param link: link of the proposal to be created
+        :type link: str
+        :return: a series of transactions to create a proposal.
+        :rtype: :class:`TransactionGroup`
+        """
+
+        params = get_default_params(self.algod)
+
+        txn0 = PaymentTxn(
+            sender=user.address,
+            params=params,
+            receiver=self.proposal_factory_address,
+            amt=4_000_000 # TODO: figure out the exact amount
+        )
+
+        txn1 = ApplicationNoOpTxn(
+            sender=user.address,
+            sp=params,
+            index=self.proposal_factory_app_id,
+            app_args=[bytes(ADMIN_STRINGS.validate_user_account, "utf-8")]
+        )
+
+        # TODO: find out if fee is correct
+        params.fee = 6000
+        txn2 = ApplicationNoOpTxn(
+            sender=user.address,
+            sp=params,
+            index=self.proposal_factory_app_id,
+            app_args=[
+                bytes(PROPOSAL_FACTORY_STRINGS.create_proposal, "utf-8"),
+                bytes(title, "utf-8"),
+                bytes(link, "utf-8")
+            ],
+            accounts=[user.address],
+            foreign_apps=[self.governance_client.voting_escrow.app_id, self.proposal_template_app_id, self.admin_app_id]
+        )
+
+        return TransactionGroup([txn0, txn1, txn2])
