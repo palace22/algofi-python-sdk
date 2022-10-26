2d33a80521e43cba1fed13f67e2c898a19024057
John Clarke <john@algofi.org>
2022-10-26 06:07:53 +0000

    added indexer functions for querying governance accounts

--- algofipy/governance/v1/governance_client.py
+++ algofipy/governance/v1/governance_client.py
@@ -7,7 +7,8 @@ from base64 import b64encode, b64decode
 # INTERFACE
 from algofipy.globals import Network
 from algofipy.transaction_utils import get_default_params, TransactionGroup
-from algofipy.governance.v1.governance_config import GOVERNANCE_CONFIGS, ADMIN_STRINGS, REWARDS_MANAGER_STRINGS
+from algofipy.state_utils import format_state
+from algofipy.governance.v1.governance_config import GOVERNANCE_CONFIGS, ADMIN_STRINGS, REWARDS_MANAGER_STRINGS, VOTING_ESCROW_STRINGS, PROPOSAL_STRINGS
 from algofipy.governance.v1.admin import Admin
 from algofipy.governance.v1.voting_escrow import VotingEscrow
 from algofipy.governance.v1.rewards_manager import RewardsManager
@@ -116,34 +117,108 @@ class GovernanceClient:
 
         return TransactionGroup([txn0, txn1, txn2, txn3, txn4])
     
-    def get_governors(self):
-        """Function that uses indexer to query for governors (users opted into the admin contract).
-
-        :return: list of governor addresses
-        :rtype: list
+    def get_governor_admin_state(self):
+        """Function that uses indexer to query for governors' admin state
         """
 
-        STORAGE_ACCOUNT_KEY_B64 = b64encode(bytes(ADMIN_STRINGS.storage_account, "utf-8")).decode("utf-8")
         # query all users opted into admin contract
         admin_app_id = self.governance_config.admin_app_id
         next_page = ""
-        tot_users = []
+        admin_app_accounts = []
         while next_page != None:
             users = self.indexer.accounts(next_page=next_page, limit=1000, application_id=admin_app_id, exclude="assets,created-apps,created-assets")
             if len(users.get("accounts",[])):
-                tot_users.extend(users["accounts"])
+                admin_app_accounts.extend(users["accounts"])
             if users.get("next-token", None):
                 next_page = users["next-token"]
             else:
                 next_page = None
+
         # filter to accounts with relevant key
-        tot_users_filtered = []
-        for user in tot_users:
+        user_data = {}
+        storage_mapping = {}
+        for user in admin_app_accounts:
             user_local_state = user.get("apps-local-state", {})
             for app_local_state in user_local_state:
                 if app_local_state["id"] == admin_app_id:
-                    kvs = app_local_state.get("key-value", [])
-                    for kv in kvs:
-                        if kv.get("key", None) == STORAGE_ACCOUNT_KEY_B64:
-                            tot_users_filtered.append(user["address"])
-        return tot_users_filtered
+                    formatted_state = format_state(app_local_state.get("key-value", []))
+                    user_account = encode_address(b64decode(formatted_state.get(ADMIN_STRINGS.user_account, "")))
+                    if user_account:
+                        open_to_delegation = formatted_state.get(ADMIN_STRINGS.open_to_delegation, "")
+                        delegating_to = encode_address(b64decode(formatted_state.get(ADMIN_STRINGS.delegating_to, "")))
+                        user_data[user_account] = {
+                            "storage_account": user["address"],
+                            "open_to_delegation": open_to_delegation,
+                            "delegating_to": delegating_to
+                        }
+                        storage_mapping[user["address"]] = user_account
+        return (user_data, storage_mapping)
+    
+    def get_governor_voting_escrow_state(self):
+        """Function that uses indexer to query for governors' voting escrow state
+        """
+
+        # query all users opted into admin contract
+        voting_escrow_app_id = self.governance_config.voting_escrow_app_id
+        next_page = ""
+        voting_escrow_app_accounts = []
+        while next_page != None:
+            users = self.indexer.accounts(next_page=next_page, limit=1000, application_id=voting_escrow_app_id, exclude="assets,created-apps,created-assets")
+            if len(users.get("accounts",[])):
+                voting_escrow_app_accounts.extend(users["accounts"])
+            if users.get("next-token", None):
+                next_page = users["next-token"]
+            else:
+                next_page = None
+
+        # filter to accounts with relevant key
+        user_data = {}
+        for user in voting_escrow_app_accounts:
+            user_local_state = user.get("apps-local-state", {})
+            for app_local_state in user_local_state:
+                if app_local_state["id"] == voting_escrow_app_id:
+                    formatted_state = format_state(app_local_state.get("key-value", []))
+                    lock_start_time = formatted_state.get(VOTING_ESCROW_STRINGS.user_lock_start_time, 0)
+                    amount_locked = formatted_state.get(VOTING_ESCROW_STRINGS.user_amount_locked, 0)
+                    lock_duration = formatted_state.get(VOTING_ESCROW_STRINGS.user_lock_duration, 0)
+                    amount_vebank = formatted_state.get(VOTING_ESCROW_STRINGS.user_amount_vebank, 0)
+                    boost_multiplier = formatted_state.get(VOTING_ESCROW_STRINGS.user_boost_multiplier, 0)
+                    user_data[user["address"]] = {
+                        VOTING_ESCROW_STRINGS.user_lock_start_time: lock_start_time,
+                        VOTING_ESCROW_STRINGS.user_amount_locked: amount_locked,
+                        VOTING_ESCROW_STRINGS.user_lock_duration: lock_duration,
+                        VOTING_ESCROW_STRINGS.user_amount_vebank: amount_vebank,
+                        VOTING_ESCROW_STRINGS.user_boost_multiplier: boost_multiplier
+                    }
+        return user_data
+
+    def get_governor_proposal_state(self, proposal_app_id):
+        """Function that uses indexer to query for governors' proposal state
+        """
+
+        # query all users opted into admin contract
+        next_page = ""
+        proposal_app_accounts = []
+        while next_page != None:
+            users = self.indexer.accounts(next_page=next_page, limit=1000, application_id=proposal_app_id, exclude="assets,created-apps,created-assets")
+            if len(users.get("accounts",[])):
+                proposal_app_accounts.extend(users["accounts"])
+            if users.get("next-token", None):
+                next_page = users["next-token"]
+            else:
+                next_page = None
+
+        # filter to accounts with relevant key
+        user_data = {}
+        for user in proposal_app_accounts:
+            user_local_state = user.get("apps-local-state", {})
+            for app_local_state in user_local_state:
+                if app_local_state["id"] == proposal_app_id:
+                    formatted_state = format_state(app_local_state.get("key-value", []))
+                    for_or_against = formatted_state.get(PROPOSAL_STRINGS.for_or_against, 0)
+                    voting_amount = formatted_state.get(PROPOSAL_STRINGS.voting_amount, 0)
+                    user_data[user["address"]] = {
+                        "for_or_against": for_or_against,
+                        "voting_amount": voting_amount
+                    }
+        return user_data
