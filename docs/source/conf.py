9efc4f39b6cd8606a3dbc28d5caa9a96a244f2e3
John Clarke <john@algofi.org>
2023-01-15 00:21:34 +0000

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, target_dir)
 # -- Project information -----------------------------------------------------
 
 project = "algofi-python-sdk"
-copyright = "2022, Algofi, Inc."
+copyright = "2023, Algofi, Inc."
 author = "Algofi, Inc."
 
 # The full version, including alpha/beta/rc tags
@@ -53,6 +53,6 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+#html_static_path = ["_static"]
 
 autoclass_content = "both"
